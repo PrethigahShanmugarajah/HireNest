8e282ad7e1e79563c6e86ce05a811afde56655ca
PrethigahShanmugarajah <prethigah@gmail.com>
2026-03-22 01:33:55 +0530

    Add job state and search filter to AppContext

--- Client/src/context/AppContext.jsx
+++ Client/src/context/AppContext.jsx
@@ -1,14 +1,38 @@
 // Client / src / context / AppContext.jsx
 /* eslint-disable react-refresh/only-export-components */
-import { createContext, useContext } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { jobsData } from "../assets/assets";
 
 export const AppContext = createContext();
 
 export const AppProvider = ({ children }) => {
   const navigate = useNavigate();
 
-  const value = { navigate };
+  const [searchFilter, setSearchFilter] = useState({
+    title: "",
+    location: "",
+  });
+  const [isSearched, setIsSearched] = useState(false);
+  const [jobs, setJobs] = useState([]);
+
+  useEffect(() => {
+    const fetchJobs = async () => {
+      setJobs(jobsData);
+    };
+
+    fetchJobs();
+  }, []);
+
+  const value = {
+    navigate,
+    searchFilter,
+    setSearchFilter,
+    isSearched,
+    setIsSearched,
+    jobs,
+    setJobs,
+  };
 
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
 };
