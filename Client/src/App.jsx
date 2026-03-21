8cd1ed39230900f554fed28bc9d557856228c6b5
PrethigahShanmugarajah <prethigah@gmail.com>
2026-03-21 22:20:25 +0530

    Add routing, Navbar, and ToastContainer to App.jsx

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,10 +1,24 @@
 // Client / src / App.jsx
+import { ToastContainer } from "react-toastify";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/client/Navbar";
+import Home from "./pages/Home/View/Home";
+import ApplyJob from "./pages/ApplyJob/View/ApplyJob";
+import Applications from "./pages/Applications/View/Applications";
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-purple-500">App</h1>
-    </div>
+    <>
+      <ToastContainer />
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/apply-job/:id" element={<ApplyJob />} />
+          <Route path="/applications" element={<Applications />} />
+        </Routes>
+      </div>
+    </>
   );
 };
 
