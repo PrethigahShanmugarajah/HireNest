fa49d3baeab83a2ed3ca0a2509ce186dd700a9d0
PrethigahShanmugarajah <prethigah@gmail.com>
2026-03-22 01:58:13 +0530

    Add Hero, JobListing, and AppDownload components to Home page

--- Client/src/pages/client/Home/View/Home.jsx
+++ Client/src/pages/client/Home/View/Home.jsx
@@ -1,9 +1,14 @@
 // Client / src / pages / client / Home / View / Home.jsx
+import AppDownload from "../Components/AppDownload";
+import Hero from "../Components/Hero";
+import JobListing from "../Components/JobListing";
 
 const Home = () => {
   return (
     <div>
-      <h1>Home</h1>
+      <Hero />
+      <JobListing />
+      <AppDownload />
     </div>
   );
 };
