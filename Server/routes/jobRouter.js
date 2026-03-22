3d93d0c5ef4074830424b9b463cb73ed5b5a7ae8
PrethigahShanmugarajah <prethigah@gmail.com>
2026-03-23 00:40:48 +0530

    Add getJobById route in jobRouter

--- Server/routes/jobRouter.js
+++ Server/routes/jobRouter.js
@@ -1,9 +1,10 @@
 // Server / routes / jobRouter.js
 import express from "express";
-import { getJobs } from "../controllers/jobController.js";
+import { getJobByID, getJobs } from "../controllers/jobController.js";
 
 const jobRouter = express.Router();
 
 jobRouter.get("/get", getJobs);
+jobRouter.get("/get/:id", getJobByID);
 
 export default jobRouter;
