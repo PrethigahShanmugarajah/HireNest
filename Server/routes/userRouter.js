9852638959c8d8a3482c4f10edf32bed728bdc8f
PrethigahShanmugarajah <prethigah@gmail.com>
2026-03-23 02:33:28 +0530

    Add applyForJob route in userRouter

--- Server/routes/userRouter.js
+++ Server/routes/userRouter.js
@@ -1,9 +1,10 @@
-// Server / routes / userRoutes.js
+// Server / routes / userRouter.js
 import express from "express";
-import { getUserData } from "../controllers/userController.js";
+import { applyForJob, getUserData } from "../controllers/userController.js";
 
 const userRouter = express.Router();
 
 userRouter.get("/me", getUserData);
+userRouter.post("/apply", applyForJob);
 
 export default userRouter;
