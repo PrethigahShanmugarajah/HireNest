69d983519844dab798d7825f56812fdccb6bb290
PrethigahShanmugarajah <prethigah@gmail.com>
2026-03-22 23:31:12 +0530

    Add companyRouter

--- Server/server.js
+++ Server/server.js
@@ -7,6 +7,7 @@ import connectDB from "./config/db.js";
 import * as Sentry from "@sentry/node";
 import { clerkWebhooks } from "./controllers/webhooks.js";
 import connectCloudinary from "./config/cloudinary.js";
+import companyRouter from "./routes/companyRouter.js";
 
 /* -------- INITIALIZE EXPRESS -------- */
 const app = express();
@@ -26,6 +27,7 @@ app.get("/debug-sentry", function mainHandler(req, res) {
   throw new Error("My first Sentry error!");
 });
 app.post("/webhooks", clerkWebhooks);
+app.use("/api/company", companyRouter);
 
 /* -------- PORT -------- */
 const port = process.env.PORT || 4000;
