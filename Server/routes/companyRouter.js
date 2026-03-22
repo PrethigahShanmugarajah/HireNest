4cd29344374f9f7a6539f332b576a9d78f3625a1
PrethigahShanmugarajah <prethigah@gmail.com>
2026-03-23 00:18:07 +0530

    Add postJob route in companyRouter

--- Server/routes/companyRouter.js
+++ Server/routes/companyRouter.js
@@ -2,13 +2,16 @@
 import express from "express";
 import {
   loginCompany,
+  postJob,
   registerCompany,
 } from "../controllers/companyController.js";
 import upload from "../config/multer.js";
+import { protectCompany } from "../middleware/authMiddleware.js";
 
 const companyRouter = express.Router();
 
 companyRouter.post("/register", upload.single("image"), registerCompany);
 companyRouter.post("/login", loginCompany);
+companyRouter.post("/post-job", protectCompany, postJob);
 
 export default companyRouter;
