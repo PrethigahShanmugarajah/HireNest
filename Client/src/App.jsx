17d46063d8d7afd9372bbe6017e5528c3f4d5bb4
PrethigahShanmugarajah <prethigah@gmail.com>
2026-03-22 15:49:16 +0530

    Add recruiter routes and RecruiterLogin modal to App

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -6,17 +6,32 @@ import Home from "./pages/client/Home/View/Home";
 import ApplyJob from "./pages/client/ApplyJob/View/ApplyJob";
 import Applications from "./pages/client/Applications/View/Applications";
 import Footer from "./components/client/Footer";
+import RecruiterLogin from "./components/recruiter/RecruiterLogin";
+import { useAppContext } from "./context/AppContext";
+import Dashboard from "./pages/recruiter/Dashboard/View/Dashboard";
+import AddJob from "./pages/recruiter/AddJob/View/AddJob";
+import ManageJobs from "./pages/recruiter/ManageJobs/View/ManageJobs";
+import ViewApplications from "./pages/recruiter/ViewApplications/View/ViewApplications";
 
 const App = () => {
+  const { showRecruiterLogin } = useAppContext();
+
   return (
     <>
       <ToastContainer />
       <div>
+        {showRecruiterLogin && <RecruiterLogin />}
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/apply-job/:id" element={<ApplyJob />} />
           <Route path="/applications" element={<Applications />} />
+
+          <Route path="/dashboard" element={<Dashboard />}>
+            <Route path="add-job" element={<AddJob />} />
+            <Route path="manage-jobs" element={<ManageJobs />} />
+            <Route path="view-applications" element={<ViewApplications />} />
+          </Route>
         </Routes>
         <Footer />
       </div>
