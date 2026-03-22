3a91546573a28a3314d4459e45ab69d3eec71015
PrethigahShanmugarajah <prethigah@gmail.com>
2026-03-22 21:30:31 +0530

    Integrate Sentry and add Clerk webhook route to the server

--- Server/server.js
+++ Server/server.js
@@ -1,8 +1,11 @@
 // Server / server.js
+import "./config/instrument.js";
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import connectDB from "./config/db.js";
+import * as Sentry from "@sentry/node";
+import { clerkWebhooks } from "./controllers/webhooks.js";
 
 /* -------- INITIALIZE EXPRESS -------- */
 const app = express();
@@ -17,9 +20,15 @@ app.use(express.urlencoded({ extended: true }));
 
 /* -------- ROUTES -------- */
 app.get("/", (req, res) => res.send("API is Working!"));
+app.get("/debug-sentry", function mainHandler(req, res) {
+  throw new Error("My first Sentry error!");
+});
+app.post("/webhooks", clerkWebhooks);
 
 /* -------- PORT -------- */
 const port = process.env.PORT || 4000;
+
+Sentry.setupExpressErrorHandler(app);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
