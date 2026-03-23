70edadaf3ca46805ca5775b615fa5076995198d9
PrethigahShanmugarajah <prethigah@gmail.com>
2026-03-23 19:55:20 +0530

    Add title input field component for add job form

---
+++ Client/src/pages/recruiter/AddJob/Components/AddJobTitleField.jsx
@@ -0,0 +1,22 @@
+// Client / src / pages / recruiter / AddJob / Components / AddJobTitleField.jsx
+import { InputField } from "../../../../components/FormField/InputField";
+
+const AddJobTitleField = ({ title, setTitle }) => {
+  return (
+    <div className="w-full max-w-lg">
+      <InputField
+        label="Job Title"
+        labelPosition="top"
+        name="title"
+        type="text"
+        placeholder="Job Title"
+        size="s"
+        value={title}
+        onChange={setTitle}
+        required
+      />
+    </div>
+  );
+};
+
+export default AddJobTitleField;
