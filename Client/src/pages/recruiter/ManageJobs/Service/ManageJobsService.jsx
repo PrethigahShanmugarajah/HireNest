6a4cf56fee82974b694a73eaed5461344f56c40b
PrethigahShanmugarajah <prethigah@gmail.com>
2026-03-24 00:55:07 +0530

    Add ManageJobsService functions for fetching, visibility toggle, and deleting jobs

---
+++ Client/src/pages/recruiter/ManageJobs/Service/ManageJobsService.jsx
@@ -0,0 +1,18 @@
+// Client / src / pages / recruiter / ManageJobs / Service / ManageJobsService.jsx
+import { fetchCompanyPostedJobs } from "../../../../services/fetch";
+import { changeJobVisibility, deleteJob } from "../../../../services/mutations";
+
+export const fetchCompanyJobsApi = async (companyToken) => {
+  const data = await fetchCompanyPostedJobs(companyToken);
+  return data?.jobsData?.slice().reverse() || [];
+};
+
+export const changeJobVisibilityApi = async (id, companyToken) => {
+  const data = await changeJobVisibility(id, companyToken);
+  return data;
+};
+
+export const deleteJobApi = async (id, companyToken) => {
+  const data = await deleteJob(id, companyToken);
+  return data;
+};
