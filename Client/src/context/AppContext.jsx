4e45950d1131f2f388fb6e998cae01f56406eaf4
PrethigahShanmugarajah <prethigah@gmail.com>
2026-03-23 19:04:14 +0530

    Persist company token and fetch company data in AppContext

--- Client/src/context/AppContext.jsx
+++ Client/src/context/AppContext.jsx
@@ -3,6 +3,7 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { jobsData } from "../assets/assets";
+import { fetchCompanyData } from "../services/fetch";
 
 export const AppContext = createContext();
 
@@ -17,7 +18,10 @@ export const AppProvider = ({ children }) => {
   const [isSearched, setIsSearched] = useState(false);
   const [jobs, setJobs] = useState([]);
   const [showRecruiterLogin, setShowRecruiterLogin] = useState(false);
-  const [companyToken, setCompanyToken] = useState(null);
+  // const [companyToken, setCompanyToken] = useState(null);
+  const [companyToken, setCompanyToken] = useState(() => {
+    return localStorage.getItem("companyToken") || null;
+  });
   const [companyData, setCompanyData] = useState(null);
 
   const CURRENCY = import.meta.env.VITE_CURRENCY;
@@ -29,7 +33,31 @@ export const AppProvider = ({ children }) => {
     };
 
     fetchJobs();
+
+    // const storedCompanyToken = localStorage.getItem("companyToken");
+
+    // if (storedCompanyToken) {
+    //   setCompanyToken(storedCompanyToken);
+    // }
   }, []);
+
+  useEffect(() => {
+    const fetchCompanyDataService = async () => {
+      try {
+        const data = await fetchCompanyData(companyToken);
+
+        if (data?.success) {
+          setCompanyData(data?.company);
+        }
+      } catch {
+        //
+      }
+    };
+
+    if (companyToken) {
+      fetchCompanyDataService();
+    }
+  }, [companyToken, backendUrl]);
 
   const value = {
     navigate,
