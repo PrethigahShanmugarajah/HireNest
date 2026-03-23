f5517cdf9dca5569e7385b740b46e64d51249295
PrethigahShanmugarajah <prethigah@gmail.com>
2026-03-23 19:54:38 +0530

    Add job salary input field component

---
+++ Client/src/pages/recruiter/AddJob/Components/AddJobSalaryField.jsx
@@ -0,0 +1,27 @@
+// Client / src / pages / recruiter / AddJob / Components / AddJobSalaryField.jsx
+import { InputField } from "../../../../components/FormField/InputField";
+import { useAppContext } from "../../../../context/AppContext";
+
+const AddJobSalaryField = ({ salary, setSalary }) => {
+  const { CURRENCY } = useAppContext();
+
+  return (
+    <div className="w-full sm:w-32 max-w-lg sm:max-w-2xl md:max-w-3xl lg:max-w-3xl">
+      <InputField
+        label="Job Salary"
+        labelPosition="top"
+        name="salary"
+        type="number"
+        placeholder={`${CURRENCY} 2500`}
+        size="s"
+        value={salary}
+        onChange={setSalary}
+        min={0}
+        required
+        labelClassName="mt-2"
+      />
+    </div>
+  );
+};
+
+export default AddJobSalaryField;
