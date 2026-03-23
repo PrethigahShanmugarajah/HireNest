444d9f266119e60db4764b53714ec3d1b258b558
PrethigahShanmugarajah <prethigah@gmail.com>
2026-03-23 19:54:13 +0530

    Add job description editor component

---
+++ Client/src/pages/recruiter/AddJob/Components/AddJobDescriptionEditor.jsx
@@ -0,0 +1,17 @@
+// Client / src / pages / recruiter / AddJob / Components / AddJobDescriptionEditor.jsx
+
+const AddJobDescriptionEditor = ({ editorRef }) => {
+  return (
+    <div className="w-full max-w-lg sm:max-w-2xl md:max-w-3xl lg:max-w-3xl">
+      <p className="my-2">
+        Job Description <span className="text-red-500 ml-1">*</span>
+      </p>
+
+      <div className="quill-focus">
+        <div ref={editorRef}></div>
+      </div>
+    </div>
+  );
+};
+
+export default AddJobDescriptionEditor;
