30e2df0500876dbdf94627fcb9d7424e36c8ca81
PrethigahShanmugarajah <prethigah@gmail.com>
2026-03-24 03:30:05 +0530

    Add edit button to manage jobs table and update action column

--- Client/src/pages/recruiter/ManageJobs/Components/ManageJobsTable.jsx
+++ Client/src/pages/recruiter/ManageJobs/Components/ManageJobsTable.jsx
@@ -3,7 +3,7 @@ import { ClipLoader } from "react-spinners";
 import { formatDate } from "../../../../utils/helpers";
 import { SingleCheckboxField } from "../../../../components/FormField/CheckboxField";
 import Button from "../../../../components/Button";
-import { Trash2 } from "lucide-react";
+import { Edit, Trash2 } from "lucide-react";
 import { Commet } from "react-loading-indicators";
 
 const ManageJobsTable = ({
@@ -14,26 +14,27 @@ const ManageJobsTable = ({
   deleteLoading,
   openVisibilityPopup,
   openDeletePopup,
+  openEditJobPage,
   pageLoading,
 }) => {
   return (
     <div className="overflow-x-auto">
       <table className="min-w-full bg-white border border-gray-300 max-sm:text-sm">
         <thead>
-          <tr>
-            <th className="py-2 px-4 border-b border-r border-gray-300 text-left max-sm:hidden">
+          <tr className="text-left">
+            <th className="py-2 px-4 border-b border-r border-gray-300 max-sm:hidden">
               #
             </th>
 
-            <th className="py-2 px-4 border-b border-r border-gray-300 text-left">
+            <th className="py-2 px-4 border-b border-r border-gray-300">
               Job Title
             </th>
 
-            <th className="py-2 px-4 border-b border-r border-gray-300 text-left max-sm:hidden">
+            <th className="py-2 px-4 border-b border-r border-gray-300 max-sm:hidden">
               Date
             </th>
 
-            <th className="py-2 px-4 border-b border-r border-gray-300 text-left max-sm:hidden">
+            <th className="py-2 px-4 border-b border-r border-gray-300 max-sm:hidden">
               Location
             </th>
 
@@ -41,11 +42,11 @@ const ManageJobsTable = ({
               Applications
             </th>
 
-            <th className="py-2 px-4 border-b border-r border-gray-300 text-left">
+            <th className="py-2 px-4 border-b border-r border-gray-300">
               Visible
             </th>
 
-            <th className="py-2 px-4 border-b border-r border-gray-300 text-left">
+            <th className="py-2 px-4 border-b border-r border-gray-300 text-center">
               Action
             </th>
           </tr>
@@ -62,12 +63,12 @@ const ManageJobsTable = ({
             </tr>
           ) : (
             paginatedData.map((job, index) => (
-              <tr key={index} className="text-black">
+              <tr key={index} className="text-black text-left">
                 <td className="py-2 px-4 border-b border-r border-gray-300 max-sm:hidden">
                   {(currentPage - 1) * itemsPerPage + index + 1}
                 </td>
 
-                <td className="py-2 px-4 border-b border-r border-gray-300">
+                <td className="py-2 px-4 border-b border-r border-gray-300 text-left">
                   {job.title}
                 </td>
 
@@ -103,92 +104,41 @@ const ManageJobsTable = ({
                   </div>
                 </td>
 
-                <td className="py-2 px-4 border-b border-gray-300 text-center">
-                  <Button
-                    variant="text"
-                    onClick={() => openDeletePopup(job)}
-                    className="border-none hover:bg-transparent"
-                    disabled={deleteLoading === job._id}
-                  >
-                    {deleteLoading === job._id ? (
-                      <div className="flex items-center justify-center">
-                        <ClipLoader size={18} color="#EF4444" />
-                      </div>
-                    ) : (
-                      <Trash2
+                <td className="border-b border-gray-300 text-center">
+                  <div className="flex items-center justify-center gap-4">
+                    <Button
+                      variant="text"
+                      onClick={() => openEditJobPage(job)}
+                      className="border-none hover:bg-transparent px-0! py-0!"
+                    >
+                      <Edit
                         size={18}
-                        className="text-red-500 hover:text-red-700"
+                        className="text-purple-500 hover:text-purple-700"
                       />
-                    )}
-                  </Button>
+                    </Button>
+
+                    <Button
+                      variant="text"
+                      onClick={() => openDeletePopup(job)}
+                      className="border-none hover:bg-transparent px-0! py-0!"
+                      disabled={deleteLoading === job._id}
+                    >
+                      {deleteLoading === job._id ? (
+                        <div className="flex items-center justify-center">
+                          <ClipLoader size={18} color="#EF4444" />
+                        </div>
+                      ) : (
+                        <Trash2
+                          size={18}
+                          className="text-red-500 hover:text-red-700"
+                        />
+                      )}
+                    </Button>
+                  </div>
                 </td>
               </tr>
             ))
           )}
-
-          {/* {paginatedData.map((job, index) => (
-            <tr key={index} className="text-black">
-              <td className="py-2 px-4 border-b border-r border-gray-300 max-sm:hidden">
-                {(currentPage - 1) * itemsPerPage + index + 1}
-              </td>
-
-              <td className="py-2 px-4 border-b border-r border-gray-300">
-                {job.title}
-              </td>
-
-              <td className="py-2 px-4 border-b border-r border-gray-300 max-sm:hidden">
-                {formatDate(job.date)}
-              </td>
-
-              <td className="py-2 px-4 border-b border-r border-gray-300 max-sm:hidden">
-                {job.location}
-              </td>
-
-              <td className="py-2 px-4 border-b border-r border-gray-300 text-center">
-                {job.applicants}
-              </td>
-
-              <td className="py-2 px-4 border-b border-r border-gray-300">
-                <div className="ml-4">
-                  {visibilityLoading === job._id ? (
-                    <div className="flex justify-start">
-                      <ClipLoader size={18} color="#A855F7" />
-                    </div>
-                  ) : (
-                    <SingleCheckboxField
-                      name={`visible-${index}`}
-                      value={job.visible}
-                      size="s"
-                      className="flex justify-start"
-                      checkboxClassName="cursor-pointer"
-                      onChange={() => openVisibilityPopup(job)}
-                      checked={job.visible}
-                    />
-                  )}
-                </div>
-              </td>
-
-              <td className="py-2 px-4 border-b border-gray-300 text-center">
-                <Button
-                  variant="text"
-                  onClick={() => openDeletePopup(job)}
-                  className="border-none hover:bg-transparent"
-                  disabled={deleteLoading === job._id}
-                >
-                  {deleteLoading === job._id ? (
-                    <div className="flex items-center justify-center">
-                      <ClipLoader size={18} color="#EF4444" />
-                    </div>
-                  ) : (
-                    <Trash2
-                      size={18}
-                      className="text-red-500 hover:text-red-700"
-                    />
-                  )}
-                </Button>
-              </td>
-            </tr>
-          ))} */}
         </tbody>
       </table>
     </div>
