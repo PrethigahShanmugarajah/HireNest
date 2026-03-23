a28dc6825b1515fd79528d4a6c1025237cfd4563
PrethigahShanmugarajah <prethigah@gmail.com>
2026-03-24 03:36:19 +0530

    Update sidebar to handle update job page and dynamic icons

--- Client/src/components/recruiter/Sidebar.jsx
+++ Client/src/components/recruiter/Sidebar.jsx
@@ -1,9 +1,15 @@
 // Client / src / components / recruiter / Sidebar.jsx
-import { Home, PlusSquare, UserRoundCheck } from "lucide-react";
+import { FilePenLine, Home, PlusSquare, UserRoundCheck } from "lucide-react";
 import { NavLink } from "react-router-dom";
+import { useAppContext } from "../../context/AppContext";
 
 const sidebarLinks = [
-  { name: "Add Job", path: "/dashboard/add-job", icon: PlusSquare },
+  {
+    name: "Add Job",
+    path: "/dashboard/add-job",
+    activePaths: ["/dashboard/add-job", "/dashboard/update-job"],
+    icon: PlusSquare,
+  },
   { name: "Manage Job", path: "/dashboard/manage-jobs", icon: Home },
   {
     name: "View Applications",
@@ -13,6 +19,8 @@ const sidebarLinks = [
 ];
 
 const Sidebar = () => {
+  const { location } = useAppContext();
+
   return (
     <div className="min-h-full border-r-2 border-gray-200 w-16 md:w-56 transition-all duration-300">
       <ul className="flex flex-col pt-5">
@@ -23,24 +31,57 @@ const Sidebar = () => {
             <NavLink
               key={index}
               to={item.path}
-              className={({ isActive }) =>
-                `flex items-center justify-center md:justify-start gap-3 p-3 md:px-6 w-full hover:bg-purple-100 ${
-                  isActive ? "bg-purple-200 border-r-4 border-purple-500" : ""
-                }`
-              }
+              // className={({ isActive }) =>
+              //   `flex items-center justify-center md:justify-start gap-3 p-3 md:px-6 w-full hover:bg-purple-100 ${
+              //     isActive ? "bg-purple-200 border-r-4 border-purple-500" : ""
+              //   }`
+              // }
+              className={() => {
+                const isCustomActive = item.activePaths
+                  ? item.activePaths.some((path) =>
+                      location.pathname.startsWith(path),
+                    )
+                  : location.pathname === item.path;
+
+                return `flex items-center justify-center md:justify-start gap-3 p-3 md:px-6 w-full hover:bg-purple-100 ${
+                  isCustomActive
+                    ? "bg-purple-200 border-r-4 border-purple-500"
+                    : ""
+                }`;
+              }}
             >
-              {({ isActive }) => (
-                <>
-                  <Icon
-                    className={`w-6 h-6 shrink-0 ${
-                      isActive ? "text-purple-500" : "text-black"
-                    }`}
-                  />
-                  <p className="hidden md:block whitespace-nowrap">
-                    {item.name}
-                  </p>
-                </>
-              )}
+              {() => {
+                const isCustomActive = item.activePaths
+                  ? item.activePaths.some((path) =>
+                      location.pathname.startsWith(path),
+                    )
+                  : location.pathname === item.path;
+
+                const isUpdatePage = location.pathname.startsWith(
+                  "/dashboard/update-job",
+                );
+
+                const DynamicIcon =
+                  item.path === "/dashboard/add-job" && isUpdatePage
+                    ? FilePenLine
+                    : Icon;
+
+                return (
+                  <>
+                    <DynamicIcon
+                      className={`w-6 h-6 shrink-0 ${
+                        isCustomActive ? "text-purple-500" : "text-black"
+                      }`}
+                    />
+
+                    <p className="hidden md:block whitespace-nowrap">
+                      {item.path === "/dashboard/add-job" && isUpdatePage
+                        ? "Update Job"
+                        : item.name}
+                    </p>
+                  </>
+                );
+              }}
             </NavLink>
           );
         })}
