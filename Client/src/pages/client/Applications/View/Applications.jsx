a473b9058eeac88fed47b4c474d4de705b707dcc
PrethigahShanmugarajah <prethigah@gmail.com>
2026-03-24 22:05:54 +0530

    Update Applications page to fetch real user data and add resume section with pagination

--- Client/src/pages/client/Applications/View/Applications.jsx
+++ Client/src/pages/client/Applications/View/Applications.jsx
@@ -1,131 +1,119 @@
 // Client / src / pages / client / Applications / View / Applications.jsx
-import { useState } from "react";
-import Button from "../../../../components/Button";
-import { jobsApplied } from "../../../../assets/assets";
-import { formatDate } from "../../../../utils/helpers";
-import { Upload } from "lucide-react";
+import { useCallback, useEffect, useState } from "react";
+import { useAppContext } from "../../../../context/AppContext";
+import { useAuth } from "@clerk/clerk-react";
+import Pagination from "../../../../components/Pagination";
+import {
+  fetchUserApplicationsApi,
+  updateUserResumeApi,
+} from "../Service/ApplicationsService";
+import ApplicationsTable from "../Components/ApplicationsTable";
+import ApplicationResumeSection from "../Components/ApplicationResumeSection";
 
 const Applications = () => {
   const [isEdit, setIsEdit] = useState(false);
-  // eslint-disable-next-line no-unused-vars
   const [resume, setResume] = useState(null);
+  const [userApplications, setUserApplications] = useState([]);
+  const [saveLoading, setSaveLoading] = useState(false);
+  const [itemsPerPage, setItemsPerPage] = useState(10);
+  const [pageLoading, setPageLoading] = useState(false);
+
+  const {
+    currentPage,
+    setCurrentPage,
+    getPaginatedData,
+    userData,
+    fetchUserDataService,
+  } = useAppContext();
+
+  const { getToken } = useAuth();
+
+  const fetchUserApplicationsService = useCallback(async () => {
+    try {
+      setPageLoading(true);
+      const token = await getToken();
+      if (!token) return;
+
+      const applications = await fetchUserApplicationsApi(token);
+      setUserApplications(applications);
+    } catch {
+      //
+    } finally {
+      setPageLoading(false);
+    }
+  }, [getToken]);
+
+  useEffect(() => {
+    fetchUserApplicationsService();
+  }, [fetchUserApplicationsService]);
+
+  const updateUserResumeService = async () => {
+    try {
+      if (!resume) return;
+      setSaveLoading(true);
+
+      const token = await getToken();
+      const data = await updateUserResumeApi(resume, token);
+
+      if (data?.success) {
+        await fetchUserDataService();
+        setIsEdit(false);
+        setResume(null);
+      }
+    } catch {
+      //
+    } finally {
+      setSaveLoading(false);
+    }
+  };
+
+  const { totalPages, paginatedData } = getPaginatedData(
+    userApplications,
+    currentPage,
+    itemsPerPage,
+  );
+
+  useEffect(() => {
+    setPageLoading(true);
+
+    const timer = setTimeout(() => {
+      setPageLoading(false);
+    }, 300);
+
+    return () => clearTimeout(timer);
+  }, [currentPage, itemsPerPage]);
 
   return (
     <div className="conatiner px-4 min-h-[65vh] 2xl:px-20 mx-auto my-10">
-      <h2 className="text-xl font-semibold">Your Resume</h2>
-      <div className="flex gap-2 mb-6 mt-3">
-        {isEdit ? (
-          <>
-            <label htmlFor="resumeUpload" className="flex items-center">
-              <p className="bg-purple-100 text-purple-600 px-4 py-2 rounded-lg mr-2">
-                Select Resume
-              </p>
+      <ApplicationResumeSection
+        isEdit={isEdit}
+        userData={userData}
+        resume={resume}
+        setResume={setResume}
+        setIsEdit={setIsEdit}
+        updateUserResumeService={updateUserResumeService}
+        saveLoading={saveLoading}
+      />
 
-              <input
-                id="resumeUpload"
-                onChange={(e) => setResume(e.target.files[0])}
-                accept="application/pdf"
-                type="file"
-                hidden
-              />
+      <ApplicationsTable
+        pageLoading={pageLoading}
+        userApplications={userApplications}
+        paginatedData={paginatedData}
+        currentPage={currentPage}
+        itemsPerPage={itemsPerPage}
+      />
 
-              <Upload className="w-9 h-9 text-white bg-purple-600 p-2 rounded-md" />
-            </label>
-
-            <Button
-              onClick={(e) => {
-                e.preventDefault();
-                setIsEdit(false);
-              }}
-              className="bg-green-100! border border-green-400  hover:bg-green-200"
-              variant={"text"}
-            >
-              Save
-            </Button>
-          </>
-        ) : (
-          <div className="flex gap-2">
-            <a
-              className="bg-purple-100 text-purple-600 px-4 py-2 rounded-lg"
-              href=""
-            >
-              Resume
-            </a>
-
-            <Button onClick={() => setIsEdit(true)} variant={"text"}>
-              Edit
-            </Button>
-          </div>
-        )}
-      </div>
-
-      <h2 className="text-xl font-semibold mb-4">Jobs Applied</h2>
-
-      <table className="min-w-full bg-white border border-gray-200 rounded-lg">
-        <thead>
-          <tr>
-            <th className="py-3 px-4 border-b border-r border-gray-200 text-left">
-              Company
-            </th>
-
-            <th className="py-3 px-4 border-b border-r border-gray-200 text-left">
-              Job Title
-            </th>
-
-            <th className="py-3 px-4 border-b border-r border-gray-200 text-left max-sm:hidden">
-              Location
-            </th>
-
-            <th className="py-3 px-4 border-b border-r border-gray-200 text-left max-sm:hidden">
-              Date
-            </th>
-
-            <th className="py-3 px-4 border-b border-r border-gray-200 text-left">
-              Status
-            </th>
-          </tr>
-        </thead>
-
-        <tbody>
-          {jobsApplied.map((job, index) =>
-            // eslint-disable-next-line no-constant-condition
-            true ? (
-              <tr key={index}>
-                <td className="py-3 px-4 flex items-center gap-2 border-b border-r border-gray-200">
-                  <img className="w-8 h-8" src={job.logo} alt="Logo" />
-                  {job.company}
-                </td>
-
-                <td className="py-2 px-4 border-b border-r border-gray-200">
-                  {job.title}
-                </td>
-
-                <td className="py-2 px-4 border-b border-r border-gray-200 max-sm:hidden">
-                  {job.location}
-                </td>
-
-                <td className="py-2 px-4 border-b border-r border-gray-200 max-sm:hidden">
-                  {formatDate(job.date)}
-                </td>
-
-                <td className="py-2 px-4 border-b border-r border-gray-200">
-                  <span
-                    className={`${
-                      job.status === "Accepted"
-                        ? "bg-green-300"
-                        : job.status === "Rejected"
-                          ? "bg-red-300"
-                          : "bg-purple-300"
-                    } px-4 py-1.5 rounded`}
-                  >
-                    {job.status}
-                  </span>
-                </td>
-              </tr>
-            ) : null,
-          )}
-        </tbody>
-      </table>
+      <Pagination
+        currentPage={currentPage}
+        totalPages={totalPages}
+        onPageChange={setCurrentPage}
+        itemsPerPage={itemsPerPage}
+        onItemsPerPageChange={(value) => {
+          setItemsPerPage(value);
+          setCurrentPage(1);
+        }}
+        itemsPerPageOptions={[5, 10, 15]}
+      />
     </div>
   );
 };
