cf611cd7b328db689fd6d031360d6342d6071891
PrethigahShanmugarajah <prethigah@gmail.com>
2026-03-24 22:07:45 +0530

    Add ApplyJobDescriptionSection to show job description and apply button

---
+++ Client/src/pages/client/ApplyJob/Components/ApplyJobDescriptionSection.jsx
@@ -0,0 +1,38 @@
+// Client / src / pages / client / ApplyJob / Components / ApplyJobDescriptionSection.jsx
+import Button from "../../../../components/Button";
+import { ClipLoader } from "react-spinners";
+
+const ApplyJobDescriptionSection = ({
+  jobData,
+  applyHandler,
+  isAlreadyApplied,
+  applyLoading,
+}) => {
+  return (
+    <div className="w-full lg:w-2/3">
+      <h2 className="font-bold text-2xl mb-4">Job Description</h2>
+      <div
+        className="rich-text"
+        dangerouslySetInnerHTML={{ __html: jobData.description }}
+      ></div>
+
+      <Button
+        onClick={applyHandler}
+        className="mt-10"
+        disabled={isAlreadyApplied || applyLoading}
+      >
+        {applyLoading ? (
+          <div className="flex items-center justify-center">
+            <ClipLoader size={18} color="#FFFFFF" />
+          </div>
+        ) : isAlreadyApplied ? (
+          "Already Applied"
+        ) : (
+          "Apply Now"
+        )}
+      </Button>
+    </div>
+  );
+};
+
+export default ApplyJobDescriptionSection;
