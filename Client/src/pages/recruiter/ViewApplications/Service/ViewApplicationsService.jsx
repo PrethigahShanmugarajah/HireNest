36cca8752d8388da414e216fe513d1cfc10326bd
PrethigahShanmugarajah <prethigah@gmail.com>
2026-03-25 03:39:10 +0530

    Add service functions for fetching applicants and changing application status

---
+++ Client/src/pages/recruiter/ViewApplications/Service/ViewApplicationsService.jsx
@@ -0,0 +1,17 @@
+// Client / src / pages / recruiter / ViewApplications / Service / ViewApplicationsService.jsx
+import { fetchCompanyJobApplicants } from "../../../../services/fetch";
+import { changeJobApplicationStatus } from "../../../../services/mutations";
+
+export const fetchCompanyJobApplicantsApi = async (companyToken) => {
+  const data = await fetchCompanyJobApplicants(companyToken);
+  return data?.applications?.slice().reverse() || [];
+};
+
+export const changeJobApplicationStatusApi = async (
+  companyToken,
+  id,
+  status,
+) => {
+  const data = await changeJobApplicationStatus(companyToken, id, status);
+  return data;
+};
