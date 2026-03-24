9571cb0065acba4bcdcb123a2c446a56bfd28c9b
PrethigahShanmugarajah <prethigah@gmail.com>
2026-03-25 03:40:38 +0530

    Implement dynamic recruiter view applications page with status change and pagination

--- Client/src/pages/recruiter/ViewApplications/View/ViewApplications.jsx
+++ Client/src/pages/recruiter/ViewApplications/View/ViewApplications.jsx
@@ -1,102 +1,172 @@
 // Client / src / pages / recruiter / ViewApplications / View / ViewApplications.jsx
-import { viewApplicationsPageData } from "../../../../assets/assets";
-import Button from "../../../../components/Button";
+import { useEffect, useState } from "react";
+import { useAppContext } from "../../../../context/AppContext";
 import { formatText } from "../../../../utils/helpers";
-import { Download, MoreHorizontal } from "lucide-react";
+import {
+  changeJobApplicationStatusApi,
+  fetchCompanyJobApplicantsApi,
+} from "../Service/ViewApplicationsService";
+import ConfirmPopup from "../../../../components/ConfirmPopup";
+import Pagination from "../../../../components/Pagination";
+import ViewApplicationsTable from "../Components/ViewApplicationsTable";
 
 const ViewApplications = () => {
+  const { companyToken, currentPage, setCurrentPage, getPaginatedData } =
+    useAppContext();
+
+  const [applicants, setApplicants] = useState(null);
+  const [statusLoading, setStatusLoading] = useState(null);
+  const [showConfirmPopup, setShowConfirmPopup] = useState(false);
+  const [selectedApplication, setSelectedApplication] = useState(null);
+  const [itemsPerPage, setItemsPerPage] = useState(10);
+  const [pageLoading, setPageLoading] = useState(false);
+
+  const fetchCompanyJobApplicantsService = async () => {
+    const applications = await fetchCompanyJobApplicantsApi(companyToken);
+    setApplicants(applications);
+  };
+
+  useEffect(() => {
+    let isMounted = true;
+
+    const getApplicants = async () => {
+      if (!companyToken) return;
+      const applications = await fetchCompanyJobApplicantsApi(companyToken);
+      if (!isMounted) return;
+      setApplicants(applications);
+    };
+
+    getApplicants();
+
+    return () => {
+      isMounted = false;
+    };
+  }, [companyToken]);
+
+  const openStatusConfirmPopup = (applicant, status) => {
+    setSelectedApplication({ ...applicant, nextStatus: status });
+    setShowConfirmPopup(true);
+  };
+
+  const changeJobApplicationStatusService = async (id, status) => {
+    try {
+      setStatusLoading(id);
+      const data = await changeJobApplicationStatusApi(
+        companyToken,
+        id,
+        status,
+      );
+
+      if (data?.success) {
+        await fetchCompanyJobApplicantsService();
+      }
+    } catch {
+      //
+    } finally {
+      setStatusLoading(null);
+    }
+  };
+
+  const validApplicants =
+    applicants?.filter((item) => item?.jobId && item?.userId) || [];
+
+  const { totalPages, paginatedData } = getPaginatedData(
+    validApplicants,
+    currentPage,
+    itemsPerPage,
+  );
+
+  useEffect(() => {
+    setPageLoading(true);
+
+    const timer = setTimeout(() => {
+      setPageLoading(false);
+    }, 300);
+
+    return () => clearTimeout(timer);
+  }, [currentPage, itemsPerPage]);
+
   return (
-    <div className="container mx-auto p-4">
+    <div className="container mx-auto p-4 max-w-6xl">
       <div>
-        <table className="w-full max-w-5xl bg-white border border-gray-200 max-sm:text-sm text-xs sm:text-sm md:text-base">
-          <thead>
-            <tr className="border-b border-gray-200">
-              <th className="py-2 px-4 text-left border-r border-gray-200">
-                #
-              </th>
+        <ViewApplicationsTable
+          applicants={applicants}
+          pageLoading={pageLoading}
+          validApplicants={validApplicants}
+          paginatedData={paginatedData}
+          currentPage={currentPage}
+          itemsPerPage={itemsPerPage}
+          statusLoading={statusLoading}
+          openStatusConfirmPopup={openStatusConfirmPopup}
+        />
 
-              <th className="py-2 px-4 text-left border-r border-gray-200">
-                User Name
-              </th>
-
-              <th className="py-2 px-4 text-left max-sm:hidden border-r border-gray-200">
-                Job Title
-              </th>
-
-              <th className="py-2 px-4 text-left max-sm:hidden border-r border-gray-200">
-                Location
-              </th>
-
-              <th className="py-2 px-4 text-left border-r border-gray-200">
-                Resume
-              </th>
-
-              <th className="py-2 px-4 text-left">Action</th>
-            </tr>
-          </thead>
-
-          <tbody>
-            {viewApplicationsPageData.map((applicant, index) => (
-              <tr className="text-black">
-                <td className="py-2 px-4 border-b border-r border-gray-200 text-center">
-                  {index + 1}
-                </td>
-
-                <td className="py-2 px-4 border-b border-r border-gray-200 text-center flex">
-                  <img
-                    className="w-10 h-10 rounded-full mr-3 max-sm:hidden"
-                    src={applicant.imgSrc}
-                    alt=""
-                  />{" "}
-                  <span>{formatText(applicant.name)}</span>{" "}
-                </td>
-
-                <td className="py-2 px-4 border-b border-r border-gray-200 max-sm:hidden">
-                  {applicant.jobTitle}
-                </td>
-
-                <td className="py-2 px-4 border-b border-r border-gray-200 max-sm:hidden">
-                  {applicant.location}
-                </td>
-
-                <td className="py-2 px-4 border-b border-r border-gray-200">
-                  <a href="" target="_blank">
-                    Resume{" "}
-                    <Download className="w-9 h-4 text-white bg-purple-500 p-0.5 rounded-md" />
-                  </a>
-                </td>
-
-                <td className="py-2 px-4 border-b border-gray-200 relative">
-                  <div className="relative inline-block text-left group">
-                    <Button
-                      variant="text"
-                      className="p-0! border-none hover:bg-none"
-                    >
-                      <MoreHorizontal className="w-5 h-5" />
-                    </Button>
-
-                    <div className="z-10 hidden absolute right-0 md:left-0 top-0 mt-2 w-32 bg-white border border-gray-200 rounded shadow group-hover:block py-1">
-                      <Button
-                        className="block w-full py-0.5! text-green-500 hover:bg-green-100 border-none"
-                        variant={"text"}
-                      >
-                        Accept
-                      </Button>
-
-                      <Button
-                        className="block w-full  py-0.5! text-red-500 hover:bg-red-100 border-none"
-                        variant={"text"}
-                      >
-                        Reject
-                      </Button>
-                    </div>
-                  </div>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+        <Pagination
+          currentPage={currentPage}
+          totalPages={totalPages}
+          onPageChange={setCurrentPage}
+          itemsPerPage={itemsPerPage}
+          onItemsPerPageChange={(value) => {
+            setItemsPerPage(value);
+            setCurrentPage(1);
+          }}
+          itemsPerPageOptions={[5, 10, 15]}
+        />
       </div>
+
+      {showConfirmPopup && selectedApplication && (
+        <ConfirmPopup
+          onClose={() => {
+            if (statusLoading) return;
+            setShowConfirmPopup(false);
+            setSelectedApplication(null);
+          }}
+          onConfirm={async () => {
+            await changeJobApplicationStatusService(
+              selectedApplication._id,
+              selectedApplication.nextStatus,
+            );
+            setShowConfirmPopup(false);
+            setSelectedApplication(null);
+          }}
+          loading={statusLoading === selectedApplication._id}
+          title={
+            selectedApplication.nextStatus === "Accept"
+              ? "Accept Application"
+              : "Reject Application"
+          }
+          description={
+            selectedApplication.nextStatus === "Accept" ? (
+              <>
+                You are about to accept the application from{" "}
+                <b>{formatText(selectedApplication?.userId?.name)}</b>.
+                <br />
+                Job: <b>{selectedApplication?.jobId?.title}</b>
+                <br />
+                Location: <b>{selectedApplication?.jobId?.location}</b>
+                <br />
+                Do you want to continue?
+              </>
+            ) : (
+              <>
+                You are about to reject the application from{" "}
+                <b>{formatText(selectedApplication?.userId?.name)}</b>.
+                <br />
+                Job: <b>{selectedApplication?.jobId?.title}</b>
+                <br />
+                Location: <b>{selectedApplication?.jobId?.location}</b>
+                <br />
+                Do you want to continue?
+              </>
+            )
+          }
+          confirmText={
+            selectedApplication.nextStatus === "Accept" ? "Accept" : "Reject"
+          }
+          confirmColor={
+            selectedApplication.nextStatus === "Accept" ? "green" : "red"
+          }
+        />
+      )}
     </div>
   );
 };
